d00c8e166f4b27b890c7bd3fa4efc7d98df71260
Александр Мирошниченко <miroshnichenko.megagroup@gmail.com>
2022-05-23 09:17:26 +0300

    Подключил antd

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+import './index.css';
+
+function Index() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default Index;
